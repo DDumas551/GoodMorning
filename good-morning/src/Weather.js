2aba84f53f101834022c124a3f17fb195c5cdfc6
DDumas551 <david.j.dumas@gmail.com>
2021-01-01 12:23:19 -0500

    Adds momentjs

--- good-morning/src/Weather.js
+++ good-morning/src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 import axios from "axios";
 
 const ALT_KEY = "330a3c4b8036f80b7b7545b90ce54a86";
@@ -26,18 +27,44 @@ const Weather = () => {
     setWeather(weatherData.data);
   };
 
+  const divStyle = { margin: "auto" };
+
   const renderCard = () => {
+    const maxTemp = weather.main.temp_max;
+    const minTemp = weather.main.temp_min;
+    const actTemp = weather.main.temp;
+    const sunrise = moment(weather.sys.sunrise).format('LT');
+    console.log(sunrise)
+
+    const displayTemp = (val) => {
+      if (temp) {
+        return (val - 273).toFixed(0);
+      } else {
+        return ((val - 273) * (9 / 5) + 32).toFixed(0);
+      }
+    };
     console.log(weather);
-    const fOrC = temp
-      ? (weather.main.temp - 273).toFixed(0)
-      : ((weather.main.temp - 273) * (9 / 5) + 32).toFixed(0);
     return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1>{`${fOrC}°${temp ? "C" : "F"}`}</h1>
-        <div>{weather.weather[0].main}</div>
-        <div>{`${weather.name}, ${weather.sys.country}`}</div>
-        <div>{`Humidity: ${weather.main.humidity}%`}</div>
-        <div>{`Current Temp: ${fOrC}°${temp ? "C" : "F"}`}</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h1 style={divStyle}>{`${displayTemp(actTemp)}°${
+          temp ? "C" : "F"
+        }`}</h1>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div>{`Min ${displayTemp(minTemp)}°`}</div>
+          <div>{`Max ${displayTemp(maxTemp)}°`}</div>
+        </div>
+        <div style={divStyle}>{weather.weather[0].main} Skies</div>
+        <div style={divStyle}>{`${weather.name}, ${weather.sys.country}`}</div>
+        <div style={divStyle}>{`Humidity: ${weather.main.humidity}%`}</div>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <div>{`Sunrise: ${weather.sys.sunrise}`}</div>
+          <div>{`Sunset: ${weather.sys.sunset}`}</div>
+        </div>
       </div>
     );
   };
